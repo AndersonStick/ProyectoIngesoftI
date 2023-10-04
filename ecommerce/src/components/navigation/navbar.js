1d337da254733130ff72856d89ed2c1f4d2db49c
Anderson Barrera <andersonbar10@gmail.com>
2023-10-04 13:18:32 -0500

    Se cambia la opcion de dashboard por historial de compras, pero aun no es funcional

--- ecommerce/src/components/navigation/navbar.js
+++ ecommerce/src/components/navigation/navbar.js
@@ -164,13 +164,13 @@ function Navbar({
             <Menu.Item>
               {({ active }) => (
                 <Link
-                  to="/dashboard"
+                  to="/"
                   className={classNames(
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                     'block px-4 py-2 text-sm'
                   )}
                 >
-                  Dashboard
+                  Historial de compras
                 </Link>
               )}
             </Menu.Item>
