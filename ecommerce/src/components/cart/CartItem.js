4c0724a3e7246924edffd222e31129355c0c6c1d
Anderson Barrera <andersonbar10@gmail.com>
2023-10-22 12:37:48 -0500

    Se finaliza CartItem

--- ecommerce/src/components/cart/CartItem.js
+++ ecommerce/src/components/cart/CartItem.js
@@ -54,11 +54,10 @@ const CartItem = ({
             <div className="flex-shrink-0">
             <img
                 src={item.product.photo}
-                alt=""
+                alt="Foto del producto"
                 className="w-24 h-24 rounded-md object-center object-cover sm:w-48 sm:h-48"
             />
             </div>
-
             <div className="ml-4 flex-1 flex flex-col justify-between sm:ml-6">
             <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
                 <div>
@@ -99,7 +98,7 @@ const CartItem = ({
                     <button 
                         type="submit"
                         className="-m-2 p-2 inline-flex text-gray-400 hover:text-gray-500">
-                        <span className="mx-2">Actualizar</span>
+                        <span className="mx-2">Actualizar cantidad</span>
                     </button>
                 </form>
                 <div className="absolute top-0 right-0">
