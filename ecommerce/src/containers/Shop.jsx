f1977790edf70b69eb9cb3be627ad4dee44c8f5d
Anderson Barrera <andersonbar10@gmail.com>
2023-10-21 19:26:31 -0500

    cambios shop.jsx

--- ecommerce/src/containers/Shop.jsx
+++ ecommerce/src/containers/Shop.jsx
@@ -319,7 +319,6 @@ const Shop = ({
                 <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                   <div className="relative z-10 flex items-baseline justify-between pt-10 pb-6 border-b border-gray-200">
                     <h1 className="text-4xl font-extrabold tracking-tight text-gray-900">Catálogo</h1>
-
                     <div className="flex items-center">              
                       <button
                         type="button"
