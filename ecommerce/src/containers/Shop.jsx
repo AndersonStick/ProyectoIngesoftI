760fff05eb888b3d952cb060a817d7c081e20ead
Anderson Barrera <andersonbar10@gmail.com>
2023-10-21 19:27:45 -0500

    arreglos shop.jsx

--- ecommerce/src/containers/Shop.jsx
+++ ecommerce/src/containers/Shop.jsx
@@ -325,7 +325,7 @@ const Shop = ({
                         className="p-2 -m-2 ml-4 sm:ml-6 text-gray-400 hover:text-gray-500 lg:hidden"
                         onClick={() => setMobileFiltersOpen(true)}
                       >
-                        <span className="sr-only">Filters</span>
+                        <span className="sr-only">Filtros</span>
                         <FilterIcon className="w-5 h-5" aria-hidden="true" />
                       </button>
                     </div>
