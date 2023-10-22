67c4510fd6c14362dd94c004985cf7540e166a5e
Anderson Barrera <andersonbar10@gmail.com>
2023-10-22 00:23:30 -0500

    Se termina caso de uso Mostrar Producto

--- ecommerce/src/containers/pages/Search.jsx
+++ ecommerce/src/containers/pages/Search.jsx
@@ -196,7 +196,6 @@ const Search = ({
                                                 </div>
                                             )
                                         })
-
                                         return result
                                     }
                                 })
