731e4bd4ebdc5370f21a2466158a0b135d4393e9
Anderson Barrera <andersonbar10@gmail.com>
2023-10-31 14:09:23 -0500

    Se cambia Country/Region* por País*

--- ecommerce/src/components/checkout/ShippingForm.js
+++ ecommerce/src/components/checkout/ShippingForm.js
@@ -170,7 +170,7 @@ const ShippingForm = ({
 
                 <div className="">
                     <label htmlFor="username" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                        Country/Region*
+                        País*
                     </label>
                     <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-gray-200 sm:pt-5">
               <div className=" sm:col-span-2">
